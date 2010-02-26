d6e91a8e556ffd739246bfc974f3a9db5b9cb2a5
dyoo <dyoo@kfisler-ra1.(none)>
2010-02-26 17:58:41 -0500

    Added skeletons for all the other cases we need to push our tests into.

--- test/tests.js
+++ test/tests.js
@@ -91,6 +91,18 @@ describe('equal', {
 	value_of(N.equals(1024, N.makeFloat(1024.0001))).should_be_false();
     },
 
+    'fixnum / complex ': function() {
+	value_of(N.equals(31337, N.makeComplex(31337))).should_be_true();
+	value_of(N.equals(31337, N.makeComplex(31337, 1))).should_be_false();
+    },
+
+    'rational / rational ' : function() {
+	value_of(N.equals(N.makeRational(23849),
+			  N.makeRational(23849))).should_be_true();
+	value_of(N.equals(N.makeRational(23849),
+			  N.makeRational(23489))).should_be_false();
+    },
+
     'rational / float': function() {
 	value_of(N.equals(N.makeRational(2),
 			  N.makeFloat(2))).should_be_true();
@@ -105,6 +117,16 @@ describe('equal', {
 			  N.makeComplex(2, 1))).should_be_false();
 	value_of(N.equals(N.makeRational(2),
 			  N.makeComplex(0, 0))).should_be_false();
+    },
+
+    'float / float': function() {
+	value_of(N.equals(N.pi, N.pi)).should_be_true();
+	value_of(N.equals(N.pi, N.e)).should_be_false();
+    },
+
+    'float / complex': function() {
+	value_of(N.equals(N.pi, N.makeComplex(N.pi, 0))).should_be_true();
+	value_of(N.equals(N.pi, N.makeComplex(N.e, 0))).should_be_false();
     },
 
     'complex / complex': function() {
@@ -156,6 +178,18 @@ describe('eqv', {
 	value_of(N.eqv(1024, N.makeFloat(1024.0001))).should_be_false();
     },
 
+    'fixnum / complex' : function() {
+	value_of(N.eqv(10, N.makeComplex(10))).should_be_false();
+	value_of(N.eqv(10, N.makeComplex(0))).should_be_false();
+    },
+
+    'rational / rational': function() {
+	value_of(N.eqv(N.makeRational(2, 3),
+		       N.makeRational(2, 3))).should_be_true();
+	value_of(N.eqv(N.makeRational(3, 2),
+		       N.makeRational(2, 3))).should_be_false();
+    },
+
     'rational / float': function() {
 	value_of(N.eqv(N.makeRational(2),
 		       N.makeFloat(2))).should_be_false();
@@ -170,6 +204,16 @@ describe('eqv', {
 		       N.makeComplex(2, 1))).should_be_false();
 	value_of(N.eqv(N.makeRational(2),
 		       N.makeComplex(0, 0))).should_be_false();
+    },
+
+    'float / float': function() {
+	value_of(N.eqv(N.pi, N.pi)).should_be_true();
+	value_of(N.eqv(N.e, N.e)).should_be_true();
+	value_of(N.eqv(N.pi, N.e)).should_be_false();
+    },
+
+    'float / complex': function() {
+	value_of(N.eqv(N.pi, N.makeComplex(N.pi))).should_be_false();
     },
 
     'complex / complex': function() {
@@ -211,176 +255,673 @@ describe('isSchemeNumber', {
 
 
 describe('isFinite', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('isRational', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('isReal', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('isExact', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('isInteger', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('toFixnum', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('toFloat', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('toExact', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('toComplex', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('add', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('subtract', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('multiply', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('divide', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('greaterThanOrEqual', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('lessThanOrEqual', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('greaterThan', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('lessThan', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('expt', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('modulo', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('numerator', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('denominator', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('sqrt', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('abs', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('floor', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
+
 });
 
 
 describe('ceiling', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('conjugate', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('magnitude', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('log', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('angle', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('atan', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('cos', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('sin', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 describe('tan', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('acos', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('asin', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('imaginaryPart', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('realPart', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('round', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('exp', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('sqr', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
 describe('gcd', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('lcm', {
+    'fixnum / fixnum' : function() {
+    },
+    'fixnum / rational' : function() {
+    },
+    'fixnum / floating' : function() {
+    },
+    'fixnum / complex' : function() {
+    },
+    'rational / rational' : function() {
+    },
+    'rational / floating' : function() {
+    },
+    'rational / complex' : function() {
+    },
+    'floating / floating' : function() {
+    },
+    'floating / complex' : function() {
+    },
+    'complex / complex' : function() {
+    }
 });
 
 
 describe('integerSqrt', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 describe('toString', {
+    'fixnums': function() {
+    },
+    'rationals': function() {
+    },
+    'floats': function() {
+    },
+    'complex': function() {
+    }
 });
 
 
